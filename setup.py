c3e8172cd84e84e4e99d0369239a54d223c7ff5f
manohar3000 <141481574+manohar3000@users.noreply.github.com>
2024-06-03 06:56:16 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,8 +6,22 @@ with open("README.md", "r") as f:
 setup(
     name="deadrin",
     version="0.0.1",
+    description="A Lame Neural Network Library",
     packages=find_packages(),
     install_requires=['numpy'],
     long_description=description,
     long_description_content_type="text/markdown",
+    url="https://github.com/manohar3000/Deadrin",
+    author="Manohar",
+    author_email="manohargehlot3000@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Intended Audience :: Information Technology",
+        "Intended Audience :: Science/Research",
+    ],
 )
